6d99341a718cbed32820503aa449722f1bf2e470
zyakal <pkd1426@gmail.com>
2022-07-11 12:18:03 +0900

    07.11 12:17

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -3,8 +3,18 @@
 </template>
 
 <script>
+import TodoHeader from "./components/todo/TodoHeader.vue";
+import TodoInput from "./components/todo/TodoInput.vue";
+import TodoList from "./components/todo/TodoList.vue";
+import TodoFooter from "./components/todo/TodoFooter.vue";
 export default {
   name: "App",
+  components: {
+    TodoHeader,
+    TodoInput,
+    TodoList,
+    TodoFooter,
+  },
 };
 </script>
 
---
+++ vue-project/src/components/todo/TodoInput.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>Input</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
